575c6cbda0d4fe2a0833641d96e77dc86b060ef0
Son Roy Almerol <github@snry.me>
2024-03-11 17:28:50 -0400

    init: htmx + sample counter

--- main.go
+++ main.go
@@ -8,14 +8,24 @@ import (
 	"github.com/sonroyaalmerol/musiqlx/pages"
 )
 
+var count = 0
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		page := pages.Hello("World")
+		page := pages.Index(count)
 		page.Render(r.Context(), w)
 	})
+
+	r.Post("/add", func(w http.ResponseWriter, r *http.Request) {
+		count++
+		page := pages.Index(count)
+		page.Render(r.Context(), w)
+	})
+
+	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	http.ListenAndServe(":3000", r)
 }
--- pages/index_templ.go
+++ pages/index_templ.go
@@ -10,7 +10,9 @@ import "context"
 import "io"
 import "bytes"
 
-func Hello(name string) templ.Component {
+import "strconv"
+
+func Index(counter int) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
@@ -23,20 +25,20 @@ func Hello(name string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div>Hello, ")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<html><head><meta charset=\"UTF-8\"><title>MusiQLx</title><script src=\"https://cdn.tailwindcss.com\"></script><script src=\"/static/js/htmx.min.js\"></script></head><body><div class=\"flex flex-col justify-center items-center h-screen\"><button class=\"border rounded p-2\" hx-post=\"/add\" hx-target=\"body\">Add</button><p>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var2 string
-		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(name)
+		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(strconv.Itoa(counter))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `pages/hello.templ`, Line: 3, Col: 19}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `pages/index.templ`, Line: 15, Col: 30}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</p></div></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
